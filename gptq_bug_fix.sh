84c98dcaabd601be2372d7ed6a2e6b84c7b56653
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2025-06-24 16:57:06 -0300

    Create gptq_bug_fix.sh

---
+++ gptq_bug_fix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=run_mistral
+#SBATCH --mem=16G
+#SBATCH -c 8
+#SBATCH -p short
+#SBATCH --gpus=1
+#SBATCH -o run_mistral.log
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=gchf@cin.ufpe.br
+
+# === Preparação ===
+module load Python3.10
+source $HOME/Offensive-Language-Analysis/mistral_tcc/bin/activate
+
+# Executa seu script Python
+pip uninstall auto-gptq
+git clone https://github.com/PanQiWei/AutoGPTQ.git
+cd AutoGPTQ
+pip install .
