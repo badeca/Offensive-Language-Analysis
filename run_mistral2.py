c31e266bd8a82e72d3b7b1dafcfefde286a635ad
Gabriela Correia Holanda Freitas <gchf@cin.ufpe.br>
2025-06-27 20:51:38 -0300

    Update run_mistral2.py

--- run_mistral2.py
+++ run_mistral2.py
@@ -90,7 +90,7 @@ current_id = 0
 print(len(sentences))
 
 for original_sentence in sentences:
-    if current_id >= 5000:
+    if current_id >= 13000:
         print(f"\n--- Processando ID {current_id} ---")
 
         original_reason = classify(original_sentence)
@@ -110,7 +110,7 @@ for original_sentence in sentences:
         time.sleep(5)  # pausa leve para segurança
     
     current_id += 1
-    if current_id == 7000:
+    if current_id == 15000:
         break
 
 # === SALVA EM XLSX ===
@@ -118,6 +118,6 @@ df = pd.DataFrame.from_dict(dataset_dict, orient='index')
 df.index.name = 'id'
 df = df.reset_index()
 
-output_file = '7000_mistral_03_gptq.xlsx'
+output_file = '15000_mistral_03_gptq.xlsx'
 df.to_excel(output_file, index=False, engine='openpyxl')
 print(f"\nArquivo salvo como {output_file}")
